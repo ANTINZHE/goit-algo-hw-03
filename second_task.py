5b6440165d382ddba6c762763fa93b3a2f0a6228
ANTIN <antonzhelihovskiy@gmail.com>
2025-07-22 02:58:07 +0300

    new massage

---
+++ second_task.py
@@ -0,0 +1,13 @@
+import random
+
+def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
+    set_of_numbers = set()
+
+    while len(set_of_numbers) < quantity:
+        set_of_numbers.add(random.randint(min, max))
+
+    list_of_numbers = list(set_of_numbers)
+
+    return sorted(list_of_numbers)
+
+print(get_numbers_ticket(1, 49, 6))
