fefcd5c0a4a603afb5b6406c9a8c461e21c57f0f
ANTIN <antonzhelihovskiy@gmail.com>
2025-07-22 03:25:53 +0300

    first task remake

--- first_task.py
+++ first_task.py
@@ -3,6 +3,7 @@ from datetime import datetime
 # Функція отримання різниці між заданою і поточною датою
 def get_days_from_today(date: str) -> int:
     today = datetime.today()
+
     try:
         formatted_date = datetime.strptime(date, '%Y-%m-%d') 
         target_days = (today - formatted_date).days
